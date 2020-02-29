bbdcafa9e6c2a616d2bad7a49b9a632a62c13b0f
devx97 <karol.czyz.97@gmail.com>
2020-02-29 16:04:50 +0100

    react fast refresh is not working actually, reverting changes, cleaning up code

--- client/src/index.js
+++ client/src/index.js
@@ -2,26 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import store from './store'
 import {Provider} from 'react-redux'
-import App from './App.js'
+import App from './App'
+import {BrowserRouter} from 'react-router-dom'
 import './styles/style.css'
 
 const rootEl = document.getElementById('root')
 
-ReactDOM.render(
-    <Provider store={store}>
-      <App/>
-    </Provider>,
-    rootEl
-)
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default
+const renderApp = () =>
     ReactDOM.render(
         <Provider store={store}>
-          <NextApp/>
+          <BrowserRouter>
+            <App/>
+          </BrowserRouter>
         </Provider>,
         rootEl
     )
-  })
+
+renderApp()
+
+if (module.hot) {
+  module.hot.accept('./App', renderApp)
 }
