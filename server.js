4cd8b918ffbfb5a6e4ef0741c6b198ef1b205c0c
Karol Czyż <karol.czyz.97@gmail.com>
2020-03-16 00:37:32 +0100

    test

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ let app = express()
 const PORT = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, 'client', 'build')))
+  app.use('static', express.static(path.join(__dirname, 'client', 'build', 'static')))
 }
 
 let server = app.listen(PORT, () => {
