d1edad659738e30547f0517717efd0d88efb5272
Karol Czyż <karol.czyz.97@gmail.com>
2020-03-16 16:58:41 +0100

    test

---
+++ client/build/precache-manifest.64e074b502859f55c24c67212491eb43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6a50c6e10d2260445fc56d6b953341",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3919000672408243ce76",
+    "url": "/static/css/main.cb5010e1.chunk.css"
+  },
+  {
+    "revision": "48908fdef4143edf25af",
+    "url": "/static/js/2.9d62b9b8.chunk.js"
+  },
+  {
+    "revision": "b15efe9942fe11e425a1a7f11e5e2760",
+    "url": "/static/js/2.9d62b9b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3919000672408243ce76",
+    "url": "/static/js/main.d6956848.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')))
+  // app.use(express.static(path.join(__dirname, 'client', 'public')))
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   });
