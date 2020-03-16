b1ac64c6ecf791a0b9f6cbde034fe71d71e2eb62
Karol Czyż <karol.czyz.97@gmail.com>
2020-03-16 18:18:46 +0100

    xD

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ let app = express()
 const PORT = process.env.PORT || 4000;
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')))
-  // app.use(express.static(path.join(__dirname, 'client', 'public')))
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  });
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
 }
 
 let server = app.listen(PORT, () => {
